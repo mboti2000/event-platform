baa6be469fff73e754d717a0152074c528aae19f
unknown <mboti20000@gmail.com>
2023-12-17 07:31:33 +0100

    intiial commit

---
+++ lib/database/models/category.model.ts
@@ -0,0 +1,14 @@
+import { Document, Schema, model, models } from "mongoose";
+
+export interface ICategory extends Document {
+  name: string;
+  _id: string;
+}
+
+const CategorySchema = new Schema({
+  name: { type: String, required: true, unique: true },
+});
+
+const Category = models.Category || model("Category", CategorySchema);
+
+export default Category;
