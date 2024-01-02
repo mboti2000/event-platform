e8d4c75ef1cb605cb1c415bd4da51c2214842593
unknown <mboti20000@gmail.com>
2024-01-02 22:21:15 +0100

    add stripe functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,17 @@
+import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
+import { getAllEvents } from "@/lib/actions/event.actions";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const events = await getAllEvents({
+    query: "",
+    category: "",
+    page: 1,
+    limit: 6,
+  });
+
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
@@ -36,6 +45,15 @@ export default function Home() {
           Trusted by <br /> Thousands of Events
         </h2>
         <div className="flex w-full gap-5 md:flex-row flex-col"></div>
+        <Collection
+          data={events?.data}
+          emptyTitle="No events found"
+          emptySubtext="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={1}
+          totalPages={2}
+        />
       </section>
     </>
   );
--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -9,7 +9,7 @@ export interface IEvent extends Document {
   imageUrl: string;
   startDateTime: Date;
   endDateTime: Date;
-  price?: string;
+  price: string;
   url?: string;
   isFree: boolean;
   category: { _id: string; name: string };
